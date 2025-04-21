6e40495b4977d1770a3c4ba47c2fb0e062e2ef8b
Herman Travis IV <49575636+herm95@users.noreply.github.com>
2025-04-21 15:44:46 -0500

    add the ability to change the button icons in the  horizontal buttons stack

--- src/cards/horizontal-buttons-stack/index.js
+++ src/cards/horizontal-buttons-stack/index.js
@@ -1,12 +1,14 @@
 import { changeConfig, changeEditor, changeLight, changeStatus, changeStyle, placeButtons, sortButtons } from './changes.js';
 import { createStructure } from './create.js';
 import { configChanged } from "../../tools/utils.js";
+import { changeIcon } from '../../components/base-card/changes.js';
 
 export function handleHorizontalButtonsStack(context) {
     if (context.cardType !== "horizontal-buttons-stack") {
         createStructure(context);
     }
 
+    changeIcon(context);
     changeStyle(context);
     sortButtons(context);
     changeConfig(context);
